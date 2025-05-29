3cce84e0588ba728e14147db02901c5e3cd583bf
Karen Luik <karen.luik5@gmail.com>
2025-05-29 19:40:32 +0200

    easter egg

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import {FooterComponent} from "./components/footer/footer.component";
@@ -12,4 +12,37 @@ import {FooterComponent} from "./components/footer/footer.component";
 })
 export class AppComponent {
   title = 'kat-cafe';
+  private konamiCode: string[] = [
+    'ArrowUp', 'ArrowUp',
+    'ArrowDown', 'ArrowDown',
+    'ArrowLeft', 'ArrowRight',
+    'ArrowLeft', 'ArrowRight',
+    'b', 'a'
+  ];
+  private inputSequence: string[] = [];
+
+  @HostListener('window:keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    this.inputSequence.push(event.key);
+    if (this.inputSequence.length > this.konamiCode.length) {
+      this.inputSequence.shift();
+    }
+
+    if (this.inputSequence.join('') === this.konamiCode.join('')) {
+      this.triggerRickroll();
+    }
+  }
+
+  triggerRickroll() {
+    const video = document.createElement('iframe');
+    video.width = '0';
+    video.height = '0';
+    video.style.display = 'none';
+    video.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1';
+    video.allow = 'autoplay';
+
+    document.body.appendChild(video);
+
+    alert('🎶 Never gonna give you up... 🎶');
+  }
 }
